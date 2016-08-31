55c09a9f0c54790aed75e1de2c68f367fcc93d3e
Réti György <gyorgy.reti@protopmail.com>
2016-09-01 01:32:36 +0200

    init

---
+++ src/js/core.js
@@ -0,0 +1,14 @@
+define([], function () {
+    window.MagyarKetfarkuKutyaNevelde = window.HungarianTwotailedDogRaiser = window.$MKKN = window.$HTDR = new function () {
+        var t = this;
+        t.version = '0.1.1';
+        t.modules = {};
+        t.dialogs = {};
+        t.inputs = {};
+        t.buttons = {};
+        t.forms = {};
+        t.tables = {};
+		t.functions = {};
+		t.data = {};
+    }();
+});
