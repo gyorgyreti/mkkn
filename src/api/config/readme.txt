172d936d4a1e46c8ae0fe5a7f0d9ff1c8b72a81a
Réti György <gyorgy.reti@protopmail.com>
2016-09-01 23:46:47 +0200

    init

---
+++ src/api/config/readme.txt
@@ -0,0 +1,10 @@
+Create a config.php in this folder. The content like this:
+
+<?php
+return [
+    'DB' => [
+        'HOST' => 'localhost',
+        'USER' => 'admin',
+        'PASS' => 'abc123',
+    ]
+];
--- src/mkkn.js
+++ src/mkkn.js
@@ -1,39 +1,5 @@
 define([
-    "js/core",
-
-    "js/elements/layout",
-
-    "js/dialogs/selectAction",
-    "js/dialogs/wait",
-    "js/dialogs/selectTrigger",
-    "js/dialogs/newAutomationName",
-    "js/dialogs/selectFormTrigger",
-
-    "js/core/init",
-    "js/core/ready",
-    "js/core/layoutReady",
-    "js/core/runTheFunctions",
-    "js/core/loadPlugins",
-    "js/core/scriptLoaded",
-
-    "js/functions/open",
-    "js/functions/close",
-    "js/functions/newDiagram",
-    "js/functions/activeElement",
-    "js/functions/widget",
-    "js/functions/title",
-
-    "js/functions/xhr/refreshCustomFields",
-    "js/functions/xhr/refreshNewsletters",
-    "js/functions/xhr/refreshForms",
-
-    "js/modules/diagram",
-    "js/modules/node",
-    "js/modules/element",
-    "js/modules/actionSelector",
-    "js/modules/actions/customField",
-    "js/modules/actions/Mail"
-], function ($AAE) {
-    console.log('%c AutomizyAutomationEditor loaded! ', 'background: #000000; color: #bada55; font-size:14px');
-    return $AAE;
+    "js/core"
+], function () {
+    $MKKN.$widget = $('<div id="mkkn"></div>').appendTo('body:first');
 });
